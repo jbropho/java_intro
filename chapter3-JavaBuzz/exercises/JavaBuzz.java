a8efcc944cf4e5ca4d4820991c0cced01eb1f71a
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-20 16:26:07 +0100

    complete chapter 3

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,5 +1,17 @@
 // can you remember how to set up your main function?
+public class JavaBuzz {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i % 15 == 0) {
+        System.out.println("JavaBuzz");
+      } if ( i % 5 == 0 ) {
+        System.out.println("Buzz");
+      } if ( i % 3 == 0 ) {
+        System.out.println("Java");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
 
-// "Java" if divisible by 3
-// "Buzz" if divisible by 5
-// "JavaBuzz" if divisible by 15
--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,12 @@
 // can you remember how to set up your main function?
 
 // loop through an integer array and print out each element
+public class LoopThroughArray {
+  public static void main(String[] args) {
+    Integer[] numbers = { 1, 2, 3, 4, 5};
+
+    for (Integer i: numbers) {
+      System.out.println(i);
+    }
+  }
+}
